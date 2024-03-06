ba7436642616bf9ff94e58bb261d78a3f48cfe7d
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-03-06 13:35:10 +0100

    feat: add starting structure to script js

---
+++ JS/script.js
@@ -0,0 +1,19 @@
+// TASK -Scrivere un programma che chieda all’utente:
+// - Il numero di chilometri da percorrere
+// - Età del passeggero
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
+// - il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// - va applicato uno sconto del 20% per i minorenni
+// - va applicato uno sconto del 40% per gli over 65.
+
+// MILESTONE 1:
+// Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra. La risposta finale (o output) sarà anch’essa da scrivere in console.
+
+//STRUCTURE:
+
+// 1-Implementare due campi input e un bottone nell'HTMTL
+// 2-Leggo tramite JS il valore inserito dall'utente nei campi input
+// 3-Calcolo il prezzo del biglietto in base ai dati letti e calcolo eventuali sconti da applicare
+    // 3-a -Calcolo del biglietto a prezzo pieno
+    // 3-b -Calcolo del biglietto a prezzo scontato del 20%
+    // 3-c -Calcolo del biglietto a prezzo scontato del 40%
